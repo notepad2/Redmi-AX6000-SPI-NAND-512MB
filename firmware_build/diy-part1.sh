c9fcb92953b79f48fec04f630a548d24af63373f
notepad2 <notepad2@live.cn>
2023-08-17 20:11:11 +0800

    Update diy-part1.sh
    # spi max frequency 52Mhz

--- firmware_build/diy-part1.sh
+++ firmware_build/diy-part1.sh
@@ -27,4 +27,4 @@ sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/
 ## 修改DTS的memory为512MB的0x20000000
 sed -i 's/reg = <0 0x40000000 0 0x10000000>/reg = <0 0x40000000 0 0x20000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
 ## 修改DTS的spi_nand的spi-max-frequency为52MHz，52000000
-sed -i 's/spi-max-frequency = <20000000>/spi-max-frequency = <52000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
+#sed -i 's/spi-max-frequency = <20000000>/spi-max-frequency = <52000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
