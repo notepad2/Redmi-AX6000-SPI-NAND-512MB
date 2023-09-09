36246e4d97ffe2740390a596267e2081d238b1a4
notepad2 <notepad2@live.cn>
2023-09-09 23:01:25 +0800

    Create add-feeds-kenzo.sh

---
+++ firmware_build/add-feeds-kenzo.sh
@@ -0,0 +1,3 @@
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
